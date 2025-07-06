e1dc6222657b3b63a2466fce5f34a7095b76615e
abrarafi <rafi.ahrijm@gmail.com>
2025-07-06 15:15:53 +0600

    completed register and login page

--- frontend/src/app/components/auth/login/login.component.ts
+++ frontend/src/app/components/auth/login/login.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -17,32 +22,50 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatInputModule,
     MatButtonModule,
     MatProgressSpinnerModule,
-    MatFormFieldModule,  ],
+    MatFormFieldModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
+
+
 export class LoginComponent implements OnInit {
-    loginForm: FormGroup;
+  loginForm: FormGroup;
   showPassword = false;
   isLoading = false;
-  emailFormControl = new FormControl('', [
-    Validators.required,
-    Validators.email,
-  ]);
-  passwordFormControl = new FormControl('', [
-    Validators.required,
-    Validators.minLength(8),
-  ]);
 
   constructor(private fb: FormBuilder) {
     this.loginForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required],
-    });
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    rememberMe: [false]
+  });
   }
+
+
   ngOnInit(): void {}
+
+
+  async onSubmit() {
+    if(this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+      this.isLoading = true;
+      // Simulate a login request
+      setTimeout(() => {
+        this.isLoading = false;
+        console.log('User logged in:', this.loginForm.value);
+      }, 2000);
+  }
 
   togglePasswordVisibility(): void {
     this.showPassword = !this.showPassword;
+  }
+  get emailFormControl(): FormControl {
+    return this.loginForm.get('email') as FormControl;
+  }
+  get passwordFormControl(): FormControl {
+    return this.loginForm.get('password') as FormControl; 
   }
 }
--- frontend/src/app/components/auth/register/register.component.ts
+++ frontend/src/app/components/auth/register/register.component.ts
@@ -1,11 +1,65 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    RouterModule,
+    CommonModule,
+    MatInputModule,
+    MatButtonModule,
+    MatProgressSpinnerModule,
+    MatFormFieldModule,
+  ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  registerForm: FormGroup;
+  showPassword = false;
+  isLoading = false;
 
+  constructor(private fb: FormBuilder, private router: RouterModule) {
+    this.registerForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+  async onSubmit() {
+   if (this.registerForm.valid) {
+     this.isLoading = true;
+     // Simulate API call
+     await new Promise((resolve) => setTimeout(resolve, 2000));
+     console.log('Registration successful');
+     this.isLoading = false;
+   }
+  }
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+  get emailFormControl(): FormControl {
+    return this.registerForm.get('email') as FormControl;
+  }
+  get passwordFormControl(): FormControl {
+    return this.registerForm.get('password') as FormControl;
+  }
+  get confirmPasswordFormControl(): FormControl {
+    return this.registerForm.get('confirmPassword') as FormControl;
+  }
 }
