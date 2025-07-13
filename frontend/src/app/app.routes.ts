d3fec9f84234524e15a7d1a782acbe4c70fa5a84
abrarafi <rafi.ahrijm@gmail.com>
2025-07-13 12:47:36 +0600

    created board view and board header

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
 import { AUTH_ROUTES } from './features/auth/auth.routes';
+import { BoardViewComponent } from './pages/board-view/board-view.component';
 
 export const routes: Routes = [
-    {
-        path: 'auth',
-        children:AUTH_ROUTES
-    }
+  {
+    path: 'auth',
+    children: AUTH_ROUTES,
+  },
+  {
+    path: 'board',
+    component: BoardViewComponent,
+    title: 'Project Board',
+  },
 ];
--- frontend/src/app/features/auth/auth.routes.ts
+++ frontend/src/app/features/auth/auth.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { BoardViewComponent } from '../../pages/board-view/board-view.component';
 
 export const AUTH_ROUTES: Routes = [
   {
