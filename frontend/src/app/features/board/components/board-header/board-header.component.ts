71f68c9055b39765af6fdfb16b0a0ca36a8738ec
abrarafi <rafi.ahrijm@gmail.com>
2025-07-15 01:24:39 +0600

    clicked ouside directive fixed

--- frontend/src/app/features/board/components/board-header/board-header.component.ts
+++ frontend/src/app/features/board/components/board-header/board-header.component.ts
@@ -6,23 +6,9 @@ import { ClickOutsideDirective } from '../../../../shared/directives/click-outsi
 
 @Component({
   selector: 'app-board-header',
-  imports: [MatIconModule, CommonModule, ],
+  imports: [MatIconModule, CommonModule, ClickOutsideDirective],
   templateUrl: './board-header.component.html',
   styleUrl: './board-header.component.scss',
-  animations: [
-    trigger('menuAnimation', [
-      transition(':enter', [
-        style({ opacity: 0, transform: 'scale(0.95)' }),
-        animate('120ms ease-out', style({ opacity: 1, transform: 'scale(1)' })),
-      ]),
-      transition(':leave', [
-        animate(
-          '100ms ease-in',
-          style({ opacity: 0, transform: 'scale(0.95)' })
-        ),
-      ]),
-    ]),
-  ],
 })
 export class BoardHeaderComponent {
   @Input() boardTitle: string | undefined = 'Board Name Not Found';
@@ -36,14 +22,13 @@ export class BoardHeaderComponent {
 
   constructor(private elementRef: ElementRef) {}
 
-
   @HostListener('document:click', ['$event'])
   onDocumentClick(event: MouseEvent) {
     if (!this.elementRef.nativeElement.contains(event.target)) {
       this.isMembersListOpen = false;
-    this.isBoardSettingsOpen = false;
-    this.isShareDropdownOpen = false;
-    this.isMenuOpen = false;
+      this.isBoardSettingsOpen = false;
+      this.isShareDropdownOpen = false;
+      this.isMenuOpen = false;
     }
   }
 
@@ -59,10 +44,7 @@ export class BoardHeaderComponent {
     // Logic to toggle board settings visibility
     console.log('Toggle board settings clicked');
   }
-  toggleShareDropdown(): void {
-    // Logic to toggle share dropdown visibility
-    console.log('Toggle share dropdown clicked');
-  }
+
   copyBoardLink(): void {
     // Logic to copy board link to clipboard
     console.log('Copy board link clicked');
@@ -83,5 +65,8 @@ export class BoardHeaderComponent {
     this.isMenuOpen = !this.isMenuOpen;
     console.log('Menu toggled:', this.isMenuOpen);
   }
-  
+  toggleShareDropdown(): void {
+    this.isShareDropdownOpen = !this.isShareDropdownOpen;
+    console.log('Share dropdown toggled:', this.isShareDropdownOpen);
+  }
 }
